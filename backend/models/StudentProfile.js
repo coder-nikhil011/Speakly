29ced6ebd10597699af4fabbe1be6ca13648da3c
Nikhil Pandit <unikhil723@gmail.com>
2026-09-23 14:24:18 +0530

    add

--- backend/models/StudentProfile.js
+++ backend/models/StudentProfile.js
@@ -4,43 +4,35 @@ const studentProfileSchema = new mongoose.Schema({
   xp: { type: Number, default: 0 },
   streak: { type: Number, default: 0 },
   lastActivityDate: { type: Date },
-  {
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-      unique: true,
-    },
-
-    level: {
-      type: String,
-      default: "Beginner",
-    },
-
-    learningGoal: {
-      type: String,
-      default: "",
-    },
-
-    wordsLearned: {
-      type: Number,
-      default: 0,
-    },
-
-    currentStreak: {
-      type: Number,
-      default: 0,
-    },
-
-    speakingSessions: {
-      type: Number,
-      default: 0,
-    },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+    unique: true,
   },
-  {
-    timestamps: true,
-  }
-);
+  level: {
+    type: String,
+    default: "Beginner",
+  },
+  learningGoal: {
+    type: String,
+    default: "",
+  },
+  wordsLearned: {
+    type: Number,
+    default: 0,
+  },
+  currentStreak: {
+    type: Number,
+    default: 0,
+  },
+  speakingSessions: {
+    type: Number,
+    default: 0,
+  },
+}, {
+  timestamps: true,
+});
 
 module.exports = mongoose.model(
   "StudentProfile",
